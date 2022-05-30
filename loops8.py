727322f2c79d5516be0cb5e857d0f9fa5d3eb707
SresthaBiswas <106442035+SresthaBiswas@users.noreply.github.com>
2022-05-30 09:29:37 +0530

    Add files via upload

---
+++ loops8.py
@@ -0,0 +1,21 @@
+#  *
+# * * * 
+#  * *
+#   *
+a=3
+for i in range (0,a):
+    b=a
+    for b in range (i,b):
+        print(" ",end="") 
+    j=1
+    for j in range (j,i):
+        print("*",end=" ")
+    print( )    
+a=3
+for i in range (0,a):
+    j=0
+    for c in range (j,i):
+        print(" ",end="")
+    for d in range (i,a):
+        print("*",end=" ")
+    print( ) 
